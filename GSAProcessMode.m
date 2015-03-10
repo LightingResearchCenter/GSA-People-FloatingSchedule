b3dfaca13355c3e62b1cd4b4a908a4536ad3e28e
Geoffrey Jones <jonesg5@rpi.edu>
2015-03-10 10:45:13 -0400

    Created class GSAProcessMode

---
+++ GSAProcessMode.m
@@ -0,0 +1,38 @@
+classdef GSAProcessMode
+    %GSAPROCESSMODE Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        mode
+    end
+    
+    methods
+        function obj = GSAProcessMode(modeStr)
+            if nargin > 0
+                switch lower(modeStr)
+                    case 'update'
+                        obj.mode = 'update';
+                    case 'overwrite'
+                        obj.mode = 'overwrite';
+                    otherwise
+                        error('Input of ''update'' or ''overwrite'' required.');
+                end
+            else
+                obj = GUISelect(obj);
+            end
+        end
+        
+        function obj = GUISelect(obj)
+            plainModeArray   = {'update','overwrite'};
+            displayModeArray = {'Update (only process new items)',...
+                'Overwrite (process all items, overwrite previous results)'};
+
+            choice = menu('Choose a process mode',displayModeArray);
+
+            obj.mode = plainModeArray{choice};
+        end
+        
+    end
+    
+end
+
