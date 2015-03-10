11aa29a3a980b1dd43e073ec324dc996aaa6a029
Geoffrey Jones <jonesg5@rpi.edu>
2015-03-10 12:09:11 -0400

    Created function LRCCompareInput

---
+++ LRCCompareInput.m
@@ -0,0 +1,29 @@
+function [newPaths,lsInput] = LRCCompareInput(inputDir,outputDir,ext)
+%LRCCOMPAREINPUT Compares list of files previously used to content of
+%inputDir
+%   Detailed explanation goes here
+
+% Find the most recent input index in the outputDir
+lsIndex = dir([outputDir,filesep,'inputIndex*.m']);
+if ~isempty(lsIndex)
+    [~,idxIndex] = max([lsIndex.datenum]);
+    lsIndex = lsIndex(idxIndex);
+    indexPath = fullfile(outputDir,lsIndex.name);
+    
+    S = load(indexPath);
+    lsOldInput = S.lsInput;
+else
+    lsOldInput = struct('name','','date','','bytes',0,'isdir',false,'datenum',0);
+end
+
+lsInput = dir([inputDir,filesep,'*',ext]);
+
+oldInputNames = {lsOldInput.name}';
+inputNames = {lsInput.name}';
+idxNew = ~ismember(inputNames,oldInputNames);
+newInputNames = inputNames(idxNew(:));
+
+newPaths = fullfile(inputDir,newInputNames);
+
+end
+
