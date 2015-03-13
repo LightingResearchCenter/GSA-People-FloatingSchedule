5e110a7baab974a67a4adf04ec9d331b32a1475e
Geoffrey Jones <jonesg5@rpi.edu>
2015-03-13 11:31:52 -0400

    Force location to be string

--- LRCImportWork.m
+++ LRCImportWork.m
@@ -16,4 +16,16 @@ location = workLogCell(2:end,2);
 startTime = datenum(workLogCell(2:end,3));
 endTime = datenum(workLogCell(2:end,4));
 
+% Make sure locations are strings
+strIdx = cellfun(@ischar,location);
+numIdx = ~strIdx;
+if any(numIdx)
+    for iLoc = 1:numel(location)
+        thisLoc = location{iLoc};
+        if ~ischar(thisLoc)
+            location{iLoc} = num2str(thisLoc);
+        end
+    end
+end
+
 end
